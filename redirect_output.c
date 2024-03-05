8b24d5bf27c2c02d9673c0a8ecac4b3ea9e89df4
Abdelmounim Sedoun <asedoun@e2r13p4.1337.ma>
2024-03-05 02:43:33 +0100

    .

---
+++ redirect_output.c
@@ -0,0 +1,22 @@
+#include "minishell.h"
+void    redirect_output(t_line **line)
+{
+	t_line *file_name;
+	int	fd;
+	int	i;
+
+	file_name = *line;
+	fd = open (file_name->str, O_RDWR | O_APPEND | O_CREAT, 0666);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		exit(0);
+	}
+	i = dup2(fd, STDOUT_FILENO);
+	if (i == -1)
+	{
+		perror("Error duplicating file descriptor");
+		close(fd);
+		exit(0);
+	}
+}
