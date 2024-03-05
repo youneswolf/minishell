ef45a206c379fcdbb1109f30ce190ad16e0f1e16
Abdelmounim Sedoun <asedoun@e2r13p4.1337.ma>
2024-03-05 02:03:25 +0100

    ..

--- echo.c
+++ echo.c
@@ -1,71 +1,141 @@
 #include "minishell.h"
 
-void exec_echo(char *str)
+char *expand(t_line **line, t_env **env)
+{
+	t_env *tmp;
+	t_line *line_tmp;
+	char special[1024];
+	special[0] = '\0';
+	int i;
+	char *var;
+	char *sub;
+	int j = 0;
+	tmp = *env;
+	line_tmp = *line;
+	i = 0;
+	if (line_tmp->str[0] == 34)
+	{
+		sub = ft_strtrim(line_tmp->str, "\"", 1);
+	}
+	else
+		sub = ft_substr(line_tmp->str, 1, ft_strlen(line_tmp->str));
+	while (sub[i])
+	{
+		if (sub[i] == '.')
+		{
+			special[j] = '.';
+			sub = ft_strtrim(sub, ".", 0);
+			j++;
+		}
+		else if (sub[i] == ',')
+		{
+			special[j] = ',';
+			sub = ft_strtrim(sub, ",", 0);
+			j++;
+		}
+		else if (sub[i] == '/')
+		{
+			special[j] = '/';
+			sub = ft_strtrim(sub, "/", 0);
+			j++;
+		}
+		else if (sub[i] == '-')
+		{
+			special[j] = '-';
+			sub = ft_strtrim(sub, "-", 0);
+			j++;
+		}
+		i++;
+	}
+	var = ft_strjoin(sub, "=");
+	while (tmp)
+	{
+		if (!ft_strncmp(var, tmp->env, ft_strlen(var)))
+		{
+			i = 0;
+			var = ft_strjoin(tmp->env,special);
+			while (var[i] != '=')
+				i++;
+			var = ft_substr(var, i + 1, ft_strlen(var) - i - 1);
+			return (var);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+
+void sub_echo(char *str)
+{
+	t_line *tmp;
+	char to_skip;
+	int sglquote;
+	int dblquote;
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (to_skip == str[i])
+		{
+			i++;
+			continue ;
+		}
+		if (str[i] == 39)
+		{
+			sglquote++;
+			if (!dblquote)
+			{
+				to_skip = str[i];
+				continue ;
+			}
+		}
+		else if (str[i] == 34)
+		{
+			dblquote++;
+			if (!sglquote)
+			{
+				to_skip =str[i];
+				continue ;
+			}
+		}
+		write(1, &str[i], 1);
+		i++;
+		}
+}
+void exec_echo(t_line **line, t_env **env)
 {
 	char *sub;
 	int sglquote;
 	int dblquote;
 	int i;
 	char to_skip;
+	t_line *tmp;
 
+	tmp = *line;
 	i = 0;
 	sglquote = 0;
 	dblquote = 0;
-	if (!ft_strncmp("echo ", str, 5))
+	if (!ft_strcmp(tmp->str, "-n"))
 	{
-		if (str[5] == '-' && str[6] == 'n')
-		{
-			sub = ft_substr(str, 8, ft_strlen(str) - 7);
-			ft_putstr_fd(sub, 1);
-			free(sub);
-		}
-		else
-		{
-			sub = ft_substr(str, 5, ft_strlen(str) - 5);
-			while(sub[i])
+			if (tmp->next->str)
 			{
-				if (sub[i] == to_skip && sub[i + 1] == ' ')
+				tmp = tmp->next;
+				if (tmp->str[0] == '$')
+					sub_echo(expand(&tmp, env));
+				else
 				{
-						while (sub[i + 1] == 32)
-							i++;
-						i++;
-						sglquote = 0;
-						dblquote = 0;
-						to_skip = '\0';
-						write(1, " ", 1);
-						continue ;
+					sub_echo(tmp->str);
 				}
-				if (to_skip == sub[i])
-				{
-					i++;
-					if (sub[i + 1] != 32)
-						continue ;
-				}
-				if (sub[i] == 39)
-				{
-					sglquote++;
-					if (!dblquote)
-					{
-						to_skip = sub[i];
-						continue ;
-					}
-				}
-				else if (sub[i] == 34)
-				{
-					dblquote++;
-					if (!sglquote)
-					{
-						to_skip = sub[i];
-						continue ;
-					}
-				}
-					write(1, &sub[i], 1);
-				i++;
 			}
-			write(1, "\n", 1);
-			free(sub);
-		}
+	}
+	else if (tmp->str[0] == '$')
+	{
+		sub_echo(expand(line, env));
+		write(1, "\n", 1);
 	}
 	else
-		return ;
+	{
+		sub_echo(tmp->str);
+		write(1, "\n", 1);
+	}
 }
